22087962a03288f613120ff4d6a0c0f0157cfc50
MikkelMiksen <mikkel.juel123@gmail.com>
2025-03-15 23:34:14 +0100

    finished the power ups they need a john to manipulate however it seems to be pretty straight forward
    healthPotion heals 80% of starting hp
    
    rubber banding sends john to his last known potion in between 1 or 3 seconds in time
    
    crash freezes johns potion in a spot for as long as the ability is active however we will not know when it happens
    
    lastly we can leash away from john healing if we get far enough while speedboost is active

--- Assets/Scripts/Player/MJ_PlayerController.cs
+++ Assets/Scripts/Player/MJ_PlayerController.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using Player;
 using UnityEngine;
 
+public static class betweenSceneLoadSaver
+{
+    
+} 
+
 public class MJ_PlayerController : MonoBehaviour, IDamageable
 {
     [Header("player properties")]
-
-    [SerializeField] private float speed,baseHP, leashSpeed, leashAbilityTime, timeSinceLeash, hp, leashDelayTime, leashDistance;
+    [SerializeField] private float speed,baseHP, leashSpeed,healthPotionCooldown, leashAbilityTime, timeSinceLeash, hp, leashCooldown, leashDistance, rubberbandCooldown, canCauseCrashCooldown;
     private Rigidbody2D rb;
-    [SerializeField] private bool leashing, canLeash;
+    [SerializeField] private bool leashing, canLeash, canRubberband, canHealthPotion, canCauseCrash, johnPosFreeze;
     private GameObject john;
-
+    private Transform lockJohnPos;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
         baseHP = hp;
         canLeash = true;
         rb = GetComponent<Rigidbody2D>();
@@ -23,7 +26,6 @@ public class MJ_PlayerController : MonoBehaviour, IDamageable
 
         canLeash = true;
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     public void DamageDeduction(float damage)
@@ -64,8 +66,84 @@ public class MJ_PlayerController : MonoBehaviour, IDamageable
                 hp = baseHP;
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && canRubberband)
+        {
+            canRubberband = false;
+            StartCoroutine(Rubberbanding());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && canCauseCrash)
+        {
+            canCauseCrash = false;
+            StartCoroutine(CrashJohn());
+        }
+
+        if (johnPosFreeze)
+        {
+            PositionLocker(john, lockJohnPos);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && canHealthPotion)
+        {
+            canHealthPotion = false;
+            StartCoroutine(DrinkHealthPotion());
+        }
     }
 
+    IEnumerator DrinkHealthPotion()
+    {
+        hp += baseHP * 0.7f;
+        yield return new WaitForSeconds(healthPotionCooldown);
+    }
+
+    void PositionLocker(GameObject targetObject, Transform targetPosition)
+    {
+        targetObject.transform.position = targetPosition.position;
+    }
+    
+    //Make John Crash
+    private IEnumerator CrashJohn()
+    {
+        lockJohnPos.position = john.transform.position;
+        yield return new WaitForSeconds(Random.Range(Random.Range(Random.Range(1,30),Random.Range(1,40)),Random.Range(Random.Range(1,20),Random.Range(1,40*2))));
+        johnPosFreeze = true;
+        yield return new WaitForSeconds(canCauseCrashCooldown);
+        canCauseCrash = true;
+        johnPosFreeze = false;
+    }
+    
+    //Rubberbanding
+    private IEnumerator Rubberbanding()
+    {
+        Transform prePos = new GameObject().transform;
+        Transform tmp = new GameObject().transform;
+        
+        prePos.position = john.transform.position;
+        
+        yield return new WaitForSeconds(Random.Range(1 , 3));
+        
+        tmp.position = john.transform.position;
+        john.transform.position = prePos.position;
+        prePos.position = tmp.position;
+        
+        yield return new WaitForSeconds(Random.Range(1 , 3));
+        
+        tmp.position = john.transform.position;
+        john.transform.position = prePos.position;
+        prePos.position = tmp.position;
+        
+        yield return new WaitForSeconds(Random.Range(1 , 3));
+        
+        tmp.position = john.transform.position;
+        john.transform.position = prePos.position;
+        
+        
+        yield return new WaitForSeconds(rubberbandCooldown);
+        canRubberband = true;
+        Debug.Log("Rubberbanding cooldown has completed!");
+    }
+    
     //Leash
     private IEnumerator Leash()
     {
@@ -76,7 +154,7 @@ public class MJ_PlayerController : MonoBehaviour, IDamageable
         RageBar.instance.AddToRageSlider(40);
         leashing = false;
         speed = 5;
-        yield return new WaitForSeconds(leashDelayTime);
+        yield return new WaitForSeconds(leashCooldown);
         canLeash = true;
     }
 }
