4539ed0235b7d59c74645f0d41852626c21a09fb
MikkelMiksen <mikkel.juel123@gmail.com>
2025-03-16 10:00:07 +0100

    yes john attacks!

--- Assets/Scripts/JohnController.cs
+++ Assets/Scripts/JohnController.cs
@@ -1,17 +1,39 @@
+using System.Collections;
+using Player;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class JohnController : MonoBehaviour
 {
+    GameObject player;
+    bool attacking = false;
+    [SerializeField] private float attackCooldown, damage, attackDistance;
     public Transform target;
     public NavMeshAgent agent;
+    
     private void Start()
     {
         agent.updateRotation = false;
         agent.updateUpAxis = false;
     }
+    
     private void Update()
     {
         agent.destination = target.position;
+
+        if (Vector2.Distance(transform.position, player.transform.position) < attackDistance && !attacking)
+        {
+            attacking = true;
+            StartCoroutine(Attacking());
+        }
+        
+    }
+
+    private IEnumerator Attacking()
+    {
+        attacking = true;
+        yield return new WaitForSeconds(attackCooldown);
+        player.GetComponent<IDamageable>().DamageDeduction(damage);
+        attacking = false;
     }
 }
